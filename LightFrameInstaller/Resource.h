4f32c3906613753ce91ba2fbee8e175527a2add8
iYoRoy <3363384229@qq.com>
2023-06-03 10:21:14 +0800

    删除不需要的资源

--- LightFrameInstaller/Resource.h
+++ LightFrameInstaller/Resource.h
@@ -4,13 +4,9 @@
 //
 #define IDC_MYICON                      2
 #define IDD_LIGHTFRAMEINSTALLER_DIALOG  102
-#define IDS_APP_TITLE                   103
-#define IDD_ABOUTBOX                    103
 #define IDM_ABOUT                       104
-#define IDM_EXIT                        105
 #define IDI_LIGHTFRAMEINSTALLER         107
 #define IDI_SMALL                       108
-#define IDC_LIGHTFRAMEINSTALLER         109
 #define IDR_MAINFRAME                   128
 #define IDR_CERT1                       129
 #define IDC_STATIC                      -1
